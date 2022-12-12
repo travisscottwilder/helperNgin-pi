a86ff54e92dcd3744d4511212cb3ea9dc4d168e9
travisscottwilder <travisscottwilder@gmail.com>
2022-12-12 13:29:11 -0800

    removed break in forloop

--- runthis.sh
+++ runthis.sh
@@ -584,29 +584,21 @@ loadConfig() {
 				echo "this is a general variable ${configVar[0]} with value ${configVar[1]}"
 
 				case ${configVar[0]} in
-					[exe_11]* ) exe_11="${configVar[1]}";
-						break;;
-					[exe_12]* ) exe_12="${configVar[1]}";
-						break;;
-					[exe_13]* ) exe_13="${configVar[1]}";
-						break;;
-					[exe_14]* ) exe_14="${configVar[1]}";
-						break;;
-					[exe_15]* ) exe_15="${configVar[1]}";
-						break;;
-					[exe_16]* ) exe_16="${configVar[1]}";
-						break;;
+					[exe_11]* ) exe_11="${configVar[1]}" ;;
+					[exe_12]* ) exe_12="${configVar[1]}" ;;
+					[exe_13]* ) exe_13="${configVar[1]}" ;;
+					[exe_14]* ) exe_14="${configVar[1]}" ;;
+					[exe_15]* ) exe_15="${configVar[1]}" ;;
+					[exe_16]* ) exe_16="${configVar[1]}" ;;
 
 
-					[userSelectedOption]* ) userSelectedOption="${configVar[1]}";
-						break;;
-					[userToUse]* ) userToUse="${configVar[1]}";
-						break;;
-					[c9userPass]* ) c9userPass="${configVar[1]}";
-						break;;
+					[userSelectedOption]* ) userSelectedOption="${configVar[1]}" ;;
+					[userToUse]* ) userToUse="${configVar[1]}" ;;
+					[c9userPass]* ) c9userPass="${configVar[1]}" ;;
+
 					
 					
-					* ) log "did not find ${configVar[0]}";;
+					* ) log "did not find ${configVar[0]}" ;;
 				esac
 			fi
 		fi
